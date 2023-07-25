5ef4b292e481ea9d86452f2189eb8499627e9829
Rehumile <sekotorehumile1@gmail.com>
2023-07-25 14:31:38 +0200

    Added logic to component to render details of podcast in preview

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import '../Card/Card.css'
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { changeDateFormat } from "../../utils/helperFunctions";
+
+export default function Card(props) {
+
+
+
+// function to seperate the genres on the page(eg. Action, True Crime, Fantasy)
+const concatenatedGenres = props.genre.reduce((accumulator, currentGenre) => {
+    if (accumulator === '') {
+      return currentGenre;
+    } else {
+      return accumulator + ', ' + currentGenre;
+    }
+  }, '');
+
+
+  
+
+
+
+
+    return (
+      <>
+    <Link style={{ textDecoration: 'none', color: '#000' }} to={`podcast/${props.item.id}`}>
+        <div onClick={() =>props.handleClick(props.item.id)} key={props.item.id} className="podcast">
+        <img src={props.item.image} className="podcast--image"/>
+        <h3 className="podcast--title">{props.item.title}</h3>
+        <p className="podcast--seasons">Seasons: {props.item.seasons}</p>
+        <p className="podcast--date" >Last Update: {changeDateFormat(props.item.updated)}</p>
+        <p className="podcast--genres">Genres: {concatenatedGenres}</p>
+    </div>
+    </Link>
+    
+</>
+
+    )
+}
+
