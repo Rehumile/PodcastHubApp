6f3cc6d2a6dc9bb852d77a98b2edd37fa7d61a33
Rehumile <sekotorehumile1@gmail.com>
2023-07-25 14:38:39 +0200

    Added logic to component for users to sort podcasts

---
+++ src/components/SortFilterComponent/SortFilter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Button, ButtonGroup } from "@mui/material";
+
+export default function SortFilter({ sortPodcast}) {
+
+
+    return (
+        <div className="filter--sort" >
+        <ButtonGroup size="small" color="primary" variant="outlined" aria-label="outlined button group">
+         <label>Sort By</label>
+               <Button onClick={()=> sortPodcast("mostRecent")}>Most Recent</Button>
+               <Button onClick={()=> sortPodcast("leastRecent")}>Least Recent</Button>
+               <Button onClick={()=> sortPodcast("titleAZ")}>a - z</Button>
+               <Button onClick={()=> sortPodcast("titleZA")}>z - a</Button>
+             </ButtonGroup>
+        </div>
+    )
+}
